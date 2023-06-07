b8fa239af581e7994f27f352e1549b454842054f
Gleb Gulin <gulingleb5@mail.ru>
2023-06-07 13:40:31 +0300

    Models

---
+++ Currencies/NLayerApp.BLL/CurrenciesViewModel.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using NLayerApp.DAL;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace NLayerApp.BLL
+{
+    public class CurrenciesViewModel : INotifyPropertyChanged
+    {
+        private Curency currency;
+        public ObservableCollection<Curency> Curencies { get; set; }
+        public event PropertyChangedEventHandler PropertyChanged;
+        public CurrenciesViewModel()
+        {
+            List<Curency> model = null;
+            //var client = new HttpClient();
+            //var task = client.GetAsync("https://api.coincap.io/v2/assets?limit=10")
+            //  .ContinueWith((taskwithresponse) =>
+            //  {
+            //      var response = taskwithresponse.Result;
+            //      string jsonString = response.Content.ReadAsStringAsync();
+            //      dynamic jsonObj = JsonConvert.DeserializeObject(jsonString);
+
+            //      Console.WriteLine(jsonString);
+            //      jsonString.Wait();
+            //      model = JsonConvert.DeserializeObject<List<Curency>>(jsonString.Result);
+
+            //  });
+            //task.Wait();
+
+            using (var client = new HttpClient())
+            {
+                using var result = client.GetAsync("https://api.coincap.io/v2/assets?limit=10");
+                string jsonString = result.Result.Content.ReadAsStringAsync().Result;
+                dynamic jsonObj2 = JsonConvert.DeserializeObject(jsonString);
+                JObject jsonObj = JObject.Parse(jsonString);
+                string name = (string)(Object)jsonObj["data"].ToString();
+                model = JsonConvert.DeserializeObject<List<Curency>>(name);
+            }
+            var a = model;
+        }
+    }
+}
---
+++ Currencies/NLayerApp.DAL/Curency.cs
@@ -0,0 +1,134 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace NLayerApp.DAL
+{
+    public class Curency : INotifyPropertyChanged
+    {
+        private string id;
+        private string rank;
+        private string symbol;
+        private string name;
+        private string supply;
+        private string maxSupply;
+        private string marketCapUsd;
+        private string volumeUsd24Hr;
+        private string changePercent24Hr;
+        private string vwap24Hr;
+        private string explorer;
+        public string Id
+        {
+            get { return id; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Id");
+            }
+        }
+        public string Rank
+        {
+            get { return rank; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Rank");
+            }
+        }
+        public string Symbol
+        {
+            get { return symbol; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Symbol");
+            }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Name");
+            }
+        }
+
+        public string Supply
+        {
+            get { return supply; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Supply");
+            }
+        }
+
+        public string MaxSupply
+        {
+            get { return maxSupply; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("MaxSupply");
+            }
+        }
+
+        public string MarketCapUsd
+        {
+            get { return marketCapUsd; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("MarketCapUsd");
+            }
+        }
+
+        public string VolumeUsd24Hr
+        {
+            get { return volumeUsd24Hr; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("VolumeUsd24Hr");
+            }
+        }
+
+        public string ChangePercent24Hr
+        {
+            get { return changePercent24Hr; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("ChangePercent24Hr");
+            }
+        }
+
+        public string Vwap24Hr
+        {
+            get { return vwap24Hr; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Vwap24Hr");
+            }
+        }
+
+        public string Explorer
+        {
+            get { return explorer; }
+            set
+            {
+                id = value;
+                OnPropertyChanged("Explorer");
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
